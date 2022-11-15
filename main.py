2ff5ef914c1ea65f3bf42682eaeb292ab0cb9169
Ciciy <occ_759566531@live.com>
2022-11-16 01:21:58 +0800

    Merge category and weight column cells

--- main.py
+++ main.py
@@ -2,13 +2,15 @@
 import math
 
 from com.excelOperation import ExcelOperations
-from com.common import read_config, get_xlsx_filenames_list
+from com.common import read_config, get_xlsx_filenames_list, basic_directory_detection
 
 
 def generate_performance_table():
     """
     生成绩效合约
     """
+    # 检测必要目录是否存在，如不存在则创建
+    basic_directory_detection()
     # 实例化考核条目维护表操作对象
     performance_item_table = ExcelOperations(read_config("path").get("performance_item_table_path"))
     # 获取存在考核条目的岗位列表
@@ -27,6 +29,7 @@ def generate_performance_table():
                 valid_items_datas = []
                 [valid_items_datas.append(key_value) for key_value in
                  performance_item_table.get_sheet_data(item_job) if key_value.get("是否生效") == 1]
+                # TODO 按类别自动将数据排序
                 # 获取当前岗位考核项目数量
                 items_num = len(valid_items_datas)
                 # 获取新行插入位置
@@ -41,10 +44,6 @@ def generate_performance_table():
                     for cell, value in zip(row, data.values()):
                         cell.value = value
                     line += 1
-                # 获取模板中预留空行数量
-                blank_rows = int(read_config("template_blank_rows").get(item_job))
-                # 模板中隐藏多余空行
-                template_tabel.hidden_data_lines(position + items_num, blank_rows - items_num)
                 # 生成绩效考核表到指定目录
                 template_tabel.save_wb("{}{}.xlsx".format(read_config("path").get("performance_folder_path"), item_job))
                 # 打开生成的表格
@@ -54,9 +53,11 @@ def generate_performance_table():
                     [new.delete_sheet(sheet) for sheet in new.get_sheet_list() if sheet != item_job]
                 except KeyError:
                     pass
-                # 获取模板中预留空行数量
+                # 获取对应模板中预留空行数量
                 blank_rows = int(read_config("template_blank_rows").get(item_job))
-                # 模板中隐藏多余空行
+                # 合并类别与权重列单元格
+                new.merge_similar_cells_by_column([1, position, position + items_num - 1], item_job, next_col=True)
+                # 隐藏多余空行
                 new.hidden_data_lines(position + items_num, blank_rows - items_num, item_job)
                 # 保存生成的表格
                 new.save_wb()
