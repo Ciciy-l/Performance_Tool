504ac1323cf7ed0a25cf90b283a536e5cb837ebf
Ciciy <occ_759566531@live.com>
2022-11-16 01:23:16 +0800

    Detect the necessary file directory method

--- com/common.py
+++ com/common.py
@@ -25,3 +25,11 @@ def get_xlsx_filenames_list(directory_path):
     [filename_list.append(filename) for parent, dirname, filename in os.walk(directory_path, topdown=True)]
     [filepath_list.append(f"{directory_path}{filename}") for filename in filename_list[0]]
     return filepath_list
+
+
+def basic_directory_detection():
+    # 检测必要文件目录，如果不存在则创建
+    path_info = read_config("path")
+    path_list = [path_info.get("performance_summary_data_path"), path_info.get("performance_folder_path")]
+    for path in path_list:
+        os.mkdir(path) if not os.path.exists(path) else None
